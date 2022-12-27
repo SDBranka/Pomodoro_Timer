9754cf623809771d42fa40b80ffe69591ecd2e47
Sam Branka <sdbranka@me.com>
2022-12-26 21:00:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Pomodoro_Timer
+
+##### Table of Contents
+
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+
+---
+
+<p float="center">
+    <img src="https://github.com/SDBranka/Miles_to_Kilometers_Converter/blob/main/Resources/screenshot0.png" width=45% alt="game start image"/>
+    <img src="https://github.com/SDBranka/Miles_to_Kilometers_Converter/blob/main/Resources/screenshot1.png" width=45% alt="game play image"/>
+</p>
+
+## Description
+
+This app aids in a users studying or working process by providing a timer based on Francesco Cirillo's Pomodoro Technique.
+
+##### Controls
+<ul>
+    <li>The user should decide on a task to be performed and then click the Start button to begin the timer
+        <ul>
+            <li>The timer label will change to "Work" and begin to run for 25 minutes during which the user should be performing their designated task</li>
+        </ul>
+    </li>
+    <li>After the 25 minute timer has run out
+        <ul>
+            <li>The app will pop out to the forefront of the screen
+                <ul>
+                    <li>Click anywhere on the window (except the close or reset button) to acknowledge and then resume working</li>
+                    <li>Note: minimizing the window will prevent this from happening</li>
+                </ul>
+            </li>
+            <li>The label will change from "Work" to "Break"</li>
+            <li>A checkmark will be added to denote how many work cycles have been completed</li>
+            <li>The app will present a 5 minute timer during which the user should take a break</li>
+        </ul>
+    </li>
+    <li>The timer will run this cycle four times and then present the user with a 20 minute long break</li>
+    <li>The timer and checkmarks may be reset at any time by clicking the reset button</li>
+</ul>
+
+##### Technologies
+
+- Python
+- Tkinter
+- Visual Studio
+
+---
+
+## How To Use
+
+Download or clone this repository to your desktop. Run main.py in an appropriate Python environment.
+
+---
+
+## References
+
+##### Continuing Work on
+- https://github.com/SDBranka/_100DOP_Exercises
+
+\
+[Back To The Top](#pomodoro-timer)
---
+++ main.py
@@ -0,0 +1,137 @@
+import tkinter
+import math
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+# WORK_MIN = 25
+# SHORT_BREAK_MIN = 5
+# LONG_BREAK_MIN = 20
+# For testing purposes
+WORK_MIN = .1
+SHORT_BREAK_MIN = .1
+LONG_BREAK_MIN = .1
+reps = 0
+timer = None
+
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+    # reactivate start button
+    start_button["state"] = "normal"
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    # disable start button after click
+    start_button["state"] = "disabled"
+
+    # convert the number of minutes into seconds
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    # print(count)
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+    # python dynamic typing (a variable can hold changeable data types ex:a = 3 a = "kkk")
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        # Make the window pop to forefront of screen
+        window.attributes('-topmost',True)
+        # Allow for window to be pushed to the background
+        window.after_idle(window.attributes,'-topmost',False)
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text = marks)
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro Timer")
+window.config(padx = 108, pady = 54, bg = YELLOW)
+
+
+# canvas
+# so that we can use an image in the window
+# highlightthickness = 0 to get rid of visible
+# border around the canvas
+canvas = tkinter.Canvas(width=200, height=226, bg = YELLOW, highlightthickness = 0)
+tomato_img = tkinter.PhotoImage(file = "tomato.png")
+canvas.create_image(100, 112, image = tomato_img)
+timer_text = canvas.create_text(100, 135, text = "00:00", 
+                    fill = "white", 
+                    font = (FONT_NAME, 35, "bold")
+)
+canvas.grid(row = 1, column = 1)
+
+#"Timer" label
+timer_label = tkinter.Label(text = "Timer", 
+                            font = ("Arial", 35, "normal"), 
+                            fg = GREEN, bg = YELLOW
+)
+timer_label.grid(row = 0, column = 1)
+
+# start button
+start_button = tkinter.Button(text = "Start",
+                                highlightthickness = 0,
+                                command = start_timer,
+                                font = ("Arial", 18, "normal"), 
+                                fg = "white", bg = "blue"
+)
+start_button.grid(row = 2, column = 0)
+
+# reset button
+reset_button = tkinter.Button(text = "Reset",
+                                highlightthickness = 0,
+                                command = reset_timer,
+                                font = ("Arial", 18, "normal"),
+                                fg = "white", bg = "blue"
+)
+reset_button.grid(row = 2, column = 2)
+
+# check marks
+# marks = ["✔", "✔"]
+marks = ""
+check_marks = tkinter.Label(text = marks, 
+                            font = ("Arial", 16, "bold"),
+                            fg = GREEN, bg = YELLOW
+)
+check_marks.grid(row = 3, column = 1)
+
+
+window.mainloop()
